22375c846683cd004304a863ed307b7f182679cf
pronit007 <91449075+pronit007@users.noreply.github.com>
2026-09-23 13:41:25 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,3 @@
-
 # Import python packages
 import streamlit as st
 from snowflake.snowpark.functions import col
@@ -70,4 +69,4 @@ if ingredients_list:
 
 import requests  
 smoothiefroot_response = requests.get("[https://my.smoothiefroot.com/api/fruit/watermelon](https://my.smoothiefroot.com/api/fruit/watermelon)")  
-st.text(smoothiefroot_response)
+st.text(smoothiefroot_response.json())
